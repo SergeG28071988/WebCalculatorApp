c6325e4e500112c432b68a3e65f38fd8ad53f9d8
Сергей Галкин <sergeygalckin2012@yandex.ru>
2021-06-05 23:51:46 +0300

    Добавьте файлы проекта.

---
+++ WebCalculator/Controllers/CalcController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using WebCalculator.Models;
+
+namespace WebCalculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new Calculator());
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Index(Calculator c, string calculate)
+        {
+            // Проверка условий
+
+            if(calculate == "add")
+            {
+                c.Result = c.No1 + c.No2;
+            }
+            else if (calculate == "sub")
+            {
+                c.Result = c.No1 - c.No2;
+            }
+            else if (calculate == "mult")
+            {
+                c.Result = c.No1 * c.No2;
+            }
+            else 
+            {
+                c.Result = c.No1 / c.No2;
+            }
+
+            return View(c);
+        }
+    }
+}
---
+++ WebCalculator/Models/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCalculator.Models
+{
+    public class Calculator
+    {
+        public int No1 { get; set; } 
+
+        public int No2 { get; set; } 
+
+        public int Result { get; set; }  
+
+    }
+}
